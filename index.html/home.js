1f186dd546051c1123f2aaafdd62c16978b9419e
biya810 <tasuku_23njr259242@n-jr.jp>
2025-11-12 19:19:56 +0900

    Add files via upload

---
+++ index.html/home.js
@@ -0,0 +1,12 @@
+// ゲーム1ボタン
+const playBtn1 = document.getElementById('play1');
+playBtn1.addEventListener('click', function() {
+  window.location.href = 'https://biya810.github.io/friends-site/';
+});
+
+// ゲーム2ボタン
+const playBtn2 = document.getElementById('play2');
+playBtn2.addEventListener('click', function() {
+  window.location.href = 'https://biya810.github.io/friends-site/';
+});
+
